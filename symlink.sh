4cae57eca2d8b818a7fdf725bc98e377c43f936c
knfujii <fujiikuninori@facebook.com>
2012-12-21 02:26:07 +0900

    bashrc
    * alias
    * rbenv
    * bundler

--- symlink.sh
+++ symlink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( vimrc gemrc gitconfig gitignore )
+DOT_FILES=( vimrc gemrc gitconfig gitignore bash_aliases bash_adding )
 
 for file in ${DOT_FILES[@]}
 do
@@ -9,3 +9,4 @@ done
 
 ln -s $HOME/dotfiles/vimfiles/ $HOME/.vim
 
+echo '. "$HOME/.bash_adding"' >> $HOME/.bashrc
